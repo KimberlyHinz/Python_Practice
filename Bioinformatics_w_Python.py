a7bd543849ef0a598b3415a0dc855eeafaeab772
Kim Hinz <kimhinz@shaw.ca>
2021-12-20 14:41:07 -0700

    Update Bioinformatics_w_Python.py

--- Bioinformatics_w_Python.py
+++ Bioinformatics_w_Python.py
@@ -27,3 +27,17 @@ Pattern = "TGATCA"
 print("The number of times " + Pattern + 
       " exists in the ori of Vibrio is: " + 
       str(PatternCount(Text, Pattern)))
+
+# %% Find the frequency of every k-mer in a sequence
+def FrequencyMap(Text, k):
+    freq = {}
+    n = len(Text)
+    for i in range(n-k+1):
+        Pattern = Text[i:i+k]
+        freq[Pattern] = 0
+        for i in range(n-k+1):
+            if Text[i:i+k] == Pattern:
+                freq[Pattern] = freq[Pattern] + 1
+    return freq
+
+print(FrequencyMap(Text, 3))
